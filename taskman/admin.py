21a12221956e8ee95ce0b7dd513687ee145bbd35
UralSultan <uralbeksultan@gmail.com>
2026-06-11 18:29:46 +0600

    Реализован первый этап домашней работы. 1. СОзданы модели 2. Зарегистрировал модели в админке 3. созданы записи 4. Сделал фикстуры с данными моделей и юзером

--- taskman/admin.py
+++ taskman/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Task, TaskType, TaskStatus
 
-# Register your models here.
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['name']
+
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['name']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type', 'created', 'updated']
+    readonly_fields = ['created', 'updated']
+    ordering = ['-created']
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('summary', 'description')
+        }),
+        ('Категоризация', {
+            'fields': ('status', 'type')
+        }),
+        ('Даты', {
+            'fields': ('created', 'updated'),
+            'classes': ('collapse',)
+        }),
+    )
--- taskman/models.py
+++ taskman/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Тип задачи'
+        verbose_name_plural = 'Типы задач'
+
+    def __str__(self):
+        return self.name
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Статус задачи'
+        verbose_name_plural = 'Статусы задач'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT, related_name='tasks',)
+    type = models.ForeignKey(TaskType, on_delete=models.PROTECT, related_name='tasks')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.summary
