85f171d836eba061d3659c592d082e7d761165de
UralSultan <uralbeksultan@gmail.com>
2026-06-22 15:38:03 +0600

    Реализовал второй этап, добавил страницы проектов и создание задач в проекте

--- taskman/urls.py
+++ taskman/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import TaskCreateView, TaskUpdateView, IndexView, TaskDetailView, TaskDeleteView
+from .views import (
+    TaskCreateView, TaskUpdateView, IndexView, TaskDetailView, TaskDeleteView,
+    ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
+)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('task/create/', TaskCreateView.as_view(), name='task_create'),
+    path('project/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('project/<int:project_pk>/task/create/', TaskCreateView.as_view(), name='task_create'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
     path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
--- taskman/views.py
+++ taskman/views.py
@@ -1,18 +1,62 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView, View
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
-from .models import Task
-from .forms import TaskModelForm
+from django.views.generic import TemplateView, View, ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from .models import Task, Project
+from .forms import TaskModelForm, ProjectModelForm
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Project
     template_name = 'index.html'
+    context_object_name = 'projects'
+    paginate_by = 5
+
+    def get_queryset(self):
+        projects = Project.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            projects = projects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        return projects
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.all()
+        context['q'] = self.request.GET.get('q', '')
         return context
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'project_detail.html'
+    context_object_name = 'project'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = self.object.tasks.all()
+        return context
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectModelForm
+    template_name = 'project_create.html'
+    success_url = reverse_lazy('index')
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    form_class = ProjectModelForm
+    template_name = 'project_update.html'
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.object.pk})
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = 'project_delete.html'
+    success_url = reverse_lazy('index')
 
 
 class TaskDetailView(TemplateView):
@@ -28,14 +72,31 @@ class TaskCreateView(CreateView):
     model = Task
     form_class = TaskModelForm
     template_name = 'task_create.html'
-    success_url = reverse_lazy('index')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.project = get_object_or_404(Project, pk=kwargs['project_pk'])
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.project = self.project
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = self.project
+        return context
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.project.pk})
 
 
 class TaskUpdateView(UpdateView):
     model = Task
     form_class = TaskModelForm
     template_name = 'task_update.html'
-    success_url = reverse_lazy('index')
+
+    def get_success_url(self):
+        return reverse('task_detail', kwargs={'pk': self.object.pk})
 
 
 class TaskDeleteView(View):
@@ -47,8 +108,9 @@ class TaskDeleteView(View):
 
     def post(self, request, *args, **kwargs):
         task = get_object_or_404(Task, pk=kwargs['pk'])
+        project_pk = task.project.pk
         task.delete()
-        return redirect('index')
+        return redirect('project_detail', pk=project_pk)
 
 
 # Create your views here.
